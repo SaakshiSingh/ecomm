a3acad6e71fc1ea6794e9cdd36f42d7c5d130232
Saakshi Singh <ashi.saakshi@gmail.com>
2021-07-26 03:29:18 +0530

    changed settings

--- Diary/celery.py
+++ Diary/celery.py
@@ -21,7 +21,7 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'reminder-':{
-        'task':'Account.task.send_notification',
+        'task':'Account.tasks.send_notification',
         'schedule':crontab(minute='*/1'),
         
     }
