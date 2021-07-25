ae5bc61445216389b33b4d2a69e41a7ff39f1a93
Saakshi Singh <ashi.saakshi@gmail.com>
2021-07-26 00:48:56 +0530

    Changed celery again

--- Diary/settings.py
+++ Diary/settings.py
@@ -206,6 +206,16 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_URL'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 django_heroku.settings(locals())
 
