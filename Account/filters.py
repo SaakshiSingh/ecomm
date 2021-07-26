eb79c3af49ba672ab5cdcee29348080429939478
Saakshi Singh <ashi.saakshi@gmail.com>
2021-07-26 20:40:30 +0530

    Final Changes

--- Account/filters.py
+++ Account/filters.py
@@ -10,7 +10,8 @@ class DateInput(forms.DateInput):
 class DiaryFilter(django_filters.FilterSet):
 	title= django_filters.CharFilter(lookup_expr='icontains',widget=forms.TextInput(attrs={'id':'searchTitle','class':'form-control ','placeholder':'Title Search'}))
 	content= django_filters.CharFilter(lookup_expr='icontains',widget=forms.TextInput(attrs={'id':'searchContent','class':'form-control ','placeholder':'Content Search'}))
+	mood= django_filters.CharFilter(lookup_expr='icontains',widget=forms.TextInput(attrs={'id':'searchMood','class':'form-control ','placeholder':'Mood Search'}))
 	date = DateFilter(field_name = 'dateCreated',lookup_expr='gte',widget=forms.TextInput(attrs={'id':'searchDate','class':'form-control datetimepicker-input','placeholder':'Diaries After the Date:','data-target':'#datetimepicker1'}))
 	class Meta:	
 		model = Diary
-		exclude =['title','content','dateCreated','picture','writer','dateModified']
+		exclude =['title','mood','content','dateCreated','picture','writer','dateModified']
